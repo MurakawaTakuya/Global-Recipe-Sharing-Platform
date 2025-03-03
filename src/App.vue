2c9776b1a1d4d340853be07a870120bce743282f
MurakawaTakuya <murakawatakuya5@gmail.com>
2025-03-03 10:07:40 +0100

    install vue-fullpage.js

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
-<script setup></script>
+<script>
+export default {
+  data() {
+    return {
+      options: {
+        anchors: ['section1', 'section2', 'section3'],
+        sectionsColor: ['#41b883', '#ff5f45', '#0798ec'],
+      },
+    }
+  },
+}
+</script>
 
 <template>
-  <header>header</header>
-
-  <main>main</main>
-
-  <footer>test</footer>
+  <full-page ref="fullpage" :options="options">
+    <!-- TODO: make component -->
+    <div class="section">
+      <!-- TODO: make component -->
+      <div class="slide">
+        <h1>hello</h1>
+        <h2>hoge fuga</h2>
+      </div>
+      <div class="slide">スライド 2</div>
+      <div class="slide">スライド 3</div>
+    </div>
+    <div class="section">Section 2: 別のコンテンツ</div>
+    <div class="section">Section 3: さらに別のコンテンツ</div>
+  </full-page>
 </template>
 
-<style scoped></style>
+<style>
+/* 必要に応じてスタイルを追加 */
+</style>
