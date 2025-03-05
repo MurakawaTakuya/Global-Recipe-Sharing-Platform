1fcb674ea75e7ddc794a4ba76f6b4139b3ab954d
MurakawaTakuya <murakawatakuya5@gmail.com>
2025-03-05 10:01:11 +0100

    install vue-rouuter

---
+++ src/components/RecipeDetail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Recipe Component</h1>
+    <p>URLパラメータ: {{ routeVariable }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecipeDetail',
+  created() {
+    console.log('Recipe Component URLパラメータ:', this.$route.params.variable)
+  },
+  computed: {
+    routeVariable() {
+      return this.$route.params.variable
+    },
+  },
+}
+</script>
+
+<style scoped>
+/* スタイルを追加する場合はここに記述 */
+</style>
