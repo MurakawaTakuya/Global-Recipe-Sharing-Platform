f6919907a46c9df9d2180ad15c7f68f9f204c593
MurakawaTakuya <murakawatakuya5@gmail.com>
2025-03-05 11:35:05 +0100

    create search router

---
+++ src/components/SearchResults.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <router-link to="/">go home</router-link>
+  </div>
+  <div>
+    <h1>Search Results for "{{ routeQuery }}"</h1>
+    <!-- 検索結果を表示するためのコードをここに追加 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchResults',
+  computed: {
+    routeQuery() {
+      return this.$route.params.query
+    },
+  },
+}
+</script>
+
+<style scoped></style>
