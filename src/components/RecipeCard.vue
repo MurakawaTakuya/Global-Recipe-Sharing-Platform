93158ef262caa0eac217952c1eb50789e3d5ea59
SotsuomeZara <sotusome1487@gmail.com>
2025-03-05 10:18:50 +0100

    fix image element

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,5 +1,8 @@
 <template>
-  <img src="/image.png" alt="">
+  <div class="img-object">
+    <img src="/image.png" alt="">
+    <p>text</p>
+  </div>
 </template>
 
 <script>
@@ -9,5 +12,16 @@ export default {
 </script>
 
 <style>
-
+  .img-object {
+    display: flex;
+    margin: 5px;
+    position: relative;
+  }
+  .img-object p {
+    display: flex;
+    position: absolute;
+    bottom: 5%;
+    left: 5%;
+    color: white;
+  }
 </style>
