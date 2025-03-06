5510284a370ac5f82ace557faf9ef4ba6ff62fe1
MurakawaTakuya <murakawatakuya5@gmail.com>
2025-03-06 10:31:00 +0100

    supabase storageと接続

---
+++ src/components/SupabaseImage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="recipe">
+    <div v-for="(instruction, index) in recipe.instructions" :key="index">
+      <p>{{ instruction.step }}</p>
+      <img :src="getImageUrl(instruction.photo)" alt="手順画像" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+import { supabase } from '../supabase'
+
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({
+  recipe: Object,
+})
+
+const getImageUrl = (filePath) => {
+  return supabase.storage.from('recipe-images').getPublicUrl(filePath).data.publicUrl
+}
+</script>
