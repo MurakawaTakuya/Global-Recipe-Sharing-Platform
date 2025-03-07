8eb7a81952d1ae517d23eff3fdfca1885b01d17b
MurakawaTakuya <murakawatakuya5@gmail.com>
2025-03-07 15:20:56 +0100

    不要なコードを削除

--- src/components/RecipeTop.vue
+++ src/components/RecipeTop.vue
@@ -5,21 +5,13 @@
   <div>
     <router-link to="/post">post</router-link>
   </div>
-
-  <el-button>I am ElButton</el-button>
-  <el-button type="primary">Primary</el-button>
-  <el-button type="success">Success</el-button>
-  <el-button type="info">Info</el-button>
-
-  <GetRecipes />
 </template>
 
 <script>
-import GetRecipes from './GetRecipes.vue';
 import RecipeCard from './RecipeCard.vue';
 export default {
   name: 'RecipeTop',
-  components: { RecipeCard, GetRecipes },
+  components: { RecipeCard },
 };
 </script>
 
