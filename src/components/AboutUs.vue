31126818338b17a7a200630d7da9bebff6cbe67e
MurakawaTakuya <murakawatakuya5@gmail.com>
2025-03-18 09:26:32 +0100

    about-usを作成

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,7 +1,112 @@
 <template>
-  <h1>About Us</h1>
+  <h1 class="about-title">About Us</h1>
+  <div class="about-list">
+    <div class="about-card">
+      <h1>Takuya Murakawa</h1>
+      <p>Nagoya Institute of Technology</p>
+      <p>Full Stack Web Developer with 6 years of experience</p>
+      <p>Computer Science, Video Image Processing, Vision Transformer</p>
+      <p>Role: Management, Design, Backend(API, Database, Storage, Authentication)</p>
+      <p><a href="https://github.com/MurakawaTakuya">GitHub Profile</a></p>
+    </div>
+
+    <div class="about-card">
+      <h1>Wakaba Nishi</h1>
+      <p>Nagoya Institute of Technology</p>
+      <p>Computer Science, Data Analysis using ABM</p>
+      <p>
+        Role: Planning the site requirements and direction, Creating the materials and recipe
+        display feature, Developing the search feature, Implementing the saved list display feature
+      </p>
+    </div>
+
+    <div class="about-card">
+      <h1>Hideki Asano</h1>
+      <p>Nagoya Institute of Technology</p>
+      <p>Computer Science, Speech Synthesis</p>
+      <p>
+        Role: Build the main page. Implement the form feature. Provide some sample recipes. Build
+        the footer and the about-us page.
+      </p>
+    </div>
+  </div>
+
+  <h1 class="about-title">Features</h1>
+  <div class="about-list">
+    <div class="about-card">
+      <h1>Search Feature</h1>
+      <p>You can search by name, category, and country.</p>
+    </div>
+
+    <div class="about-card">
+      <h1>Authentication</h1>
+      <p>You can sign in using your Google Account.</p>
+    </div>
+
+    <div class="about-card">
+      <h1>Create Post</h1>
+      <p>
+        Users can create new posts to share their thoughts and ideas. You need to sign in to create
+        a post.
+      </p>
+    </div>
+
+    <div class="about-card">
+      <h1>Rating System</h1>
+      <p>Users can rate posts to provide feedback and help others find quality content.</p>
+    </div>
+
+    <div class="about-card">
+      <h1>Save Feature</h1>
+      <p>You can save your favorite posts for easy access later.</p>
+    </div>
+
+    <div class="about-card">
+      <h1>Deployment</h1>
+      <p>Hosting: <a target="_blank" href="https://vercel.com/">Vercel</a></p>
+      <p>Database, Authentication: <a target="_blank" href="https://supabase.com/">Supabase</a></p>
+    </div>
+  </div>
 </template>
 
 <script></script>
 
-<style></style>
+<style>
+.about-title {
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+.about-list {
+  display: flex;
+  justify-content: space-around;
+  gap: 20px;
+  flex-wrap: wrap;
+  width: 95%;
+  margin: 0 auto;
+  margin-bottom: 30px;
+}
+
+.about-card {
+  width: 32%;
+  min-width: 300px;
+  border: thin solid #ebebeb;
+  border-radius: 15px;
+  padding: 15px 23px;
+  box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.12);
+  transition: all 0.3s;
+  text-decoration: none;
+  color: black;
+  position: relative;
+}
+
+.about-card p {
+  margin: 5px 0;
+}
+
+@media (max-width: 600px) {
+  .about-card {
+    width: 98%;
+  }
+}
+</style>
--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    <p>Efrei, Web Development - Group 2</p>
+    <p>Efrei Paris, Web Development 2025 - Group 2</p>
     <router-link to="/about-us">About Us</router-link>
   </footer>
 </template>
@@ -12,6 +12,14 @@ footer {
   display: flex;
   justify-content: center;
   gap: 10px;
-  margin: 8px 0 5px;
+  margin: 15px 0;
+}
+
+@media (max-width: 600px) {
+  footer {
+    gap: 0;
+    flex-direction: column;
+    align-items: center;
+  }
 }
 </style>
