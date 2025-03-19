b8e4eecda1463eb15256292e79eba29cc3e23926
MurakawaTakuya <murakawatakuya5@gmail.com>
2025-03-19 01:14:07 +0100

    GitHub RepositoryのURLを追加

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,6 +2,7 @@
   <footer>
     <p>Efrei Paris, Web Development 2025 - Group 2</p>
     <router-link to="/about-us">About Us</router-link>
+    <a href="https://github.com/MurakawaTakuya/Global-Recipe-Sharing-Platform">GitHub Repository</a>
   </footer>
 </template>
 
